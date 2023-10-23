e45c80d7e2dd1c4106807df0167b67933bb9e5d7
kruglov-ilya <kruglov-il1@yandex.ru>
2023-10-23 18:59:22 +0400

    feat: add DB connection to program file

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
+using Lab5;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connection = builder.Configuration.GetConnectionString("DefaultConnection") 
+                    ?? throw new Exception("Database connection string not set");
+// добавляем контекст ApplicationContext в качестве сервиса в приложение
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
