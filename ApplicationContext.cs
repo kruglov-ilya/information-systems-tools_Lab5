4247dc7e5db99ffc137d077196b5d4b37e22681f
kruglov-ilya <kruglov-il1@yandex.ru>
2023-10-23 18:54:23 +0400

    feat: add DB connection context class

---
+++ ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Lab5.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab5
+{
+    public sealed class ApplicationContext: DbContext
+    {
+        public DbSet<District> Districts { get; set; } = null!;
+        public DbSet<Material> Materials { get; set; } = null!;
+        public DbSet<RealEstate> RealEstates { get; set; } = null!;
+        public DbSet<Criteria> Criteria { get; set; } = null!;
+        public DbSet<Realtor> Realtors { get; set; } = null!;
+        public DbSet<Sale> Sales { get; set; } = null!;
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
